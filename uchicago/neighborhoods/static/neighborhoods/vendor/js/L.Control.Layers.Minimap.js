b0e6a311ea14d260f5a12c92e642a5633fd872af
JD Godchaux <jd@nijel.org>
2016-11-09 17:50:08 -0500

    Update L.Control.Layers.Minimap.js

--- uchicago/neighborhoods/static/neighborhoods/vendor/js/L.Control.Layers.Minimap.js
+++ uchicago/neighborhoods/static/neighborhoods/vendor/js/L.Control.Layers.Minimap.js
@@ -96,6 +96,8 @@ L.Control.Layers.Minimap = L.Control.Layers.extend({
         var name = L.DomUtil.create('span', '', span);
         name.innerHTML = ' ' + obj.name;
 
+	label._minimap.invalidateSize();
+	
         return label;
     },
 
