039d8ca5d9d1cd29c2b3731b014a9b578a826ce8
JD Godchaux <jd@nijel.org>
2016-11-09 16:05:27 -0500

    Update app.js

--- uchicago/neighborhoods/static/neighborhoods/js/app.js
+++ uchicago/neighborhoods/static/neighborhoods/js/app.js
@@ -93,9 +93,7 @@ app.createMap = function (mapId, drawnNeighborhood) {
         // create map controller
         L.control.layers.minimap(app.baseMaps[mapId], app.overlayMaps[mapId], {
             overlayBackgroundLayer: app.map[mapId].tiles,
-            topPadding: 0,
-            bottomPadding: 10,
-            collapsed: false,
+            collapsed: true,
         }).addTo(app.map[mapId]);
         //L.control.layers(app.baseMaps[mapId], app.overlayMaps[mapId]).addTo(app.map[mapId]);
 
